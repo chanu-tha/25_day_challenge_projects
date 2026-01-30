f5208c78aac467631e00a5c0ecd8eb1a25a05440
chanu-tha <160107409+chanu-tha@users.noreply.github.com>
2026-01-30 10:04:53 +0000

    add execise 04_rock_paper_scissors

--- 04_rock_paper_scissors/main.py
+++ 04_rock_paper_scissors/main.py
@@ -5,28 +5,37 @@ symbols: dict[str, str] = {'rock': '🪨',  # rock
                            'paper': '📄',  # paper
                            'scissors': '✂️'}  # scissors
 
-# Get user input:
-player_choice: str = input('Choose rock (🪨), paper (📄), or scissors (✂️): ').strip().lower()
-computer_choice: str = random.choice(tuple(symbols))
+while True:
 
-# Display choices:
-print('\nResults')
-print('----------------')
-print(f'You:      {symbols[player_choice]}  {player_choice}')
-print(f'Computer: {symbols[computer_choice]}  {computer_choice}')
-print('----------------')
+    # Get user input:
+    player_choice: str = input('Choose rock (🪨), paper (📄), or scissors (✂️): ').strip().lower()
+    computer_choice: str = random.choice(tuple(symbols))
 
-# Determine the winner
-if player_choice == computer_choice:
-    print('It\'s a tie!')
-elif player_choice == 'rock' and computer_choice == 'scissors':
-    print('You won with rock! 🪨')
-elif player_choice == 'paper' and computer_choice == 'rock':
-    print('You won with paper! 📄')
-elif player_choice == 'scissors' and computer_choice == 'paper':
-    print('You won with scissors! ✂️')
-else:
-    print('Computer wins! 🤖')
+    # Display choices:
+    print('\nResults')
+    print('----------------')
+    print(f'You:      {symbols[player_choice]}  {player_choice}')
+    print(f'Computer: {symbols[computer_choice]}  {computer_choice}')
+    print('----------------')
+
+    # Determine the winner
+    if player_choice == computer_choice:
+        print('It\'s a tie!')
+    elif player_choice == 'rock' and computer_choice == 'scissors':
+        print('You won with rock! 🪨')
+    elif player_choice == 'paper' and computer_choice == 'rock':
+        print('You won with paper! 📄')
+    elif player_choice == 'scissors' and computer_choice == 'paper':
+        print('You won with scissors! ✂️')
+    else:
+        print('Computer wins! 🤖')
+
+    print('----------------')
+    player_retries = input("Play again? Y/N ").lower()
+
+    if player_retries != "y":
+        print("Thanks for playing")
+        break
 
 # Homework:
 # 1. Make this code loop indefinitely so that you can play forever without
